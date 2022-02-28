e4f0503605d28a474082ea57aa9a4b8dba6b3be4
AmirBesharati <amirbesharati59@gmail.com>
2022-02-28 18:28:05 +0330

    #seeders and factories created

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Product;
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Product::class, function (Faker $faker) {
+
+    $discounts = [
+      0 , 100 , 400 , 1000 , 1200
+    ];
+    return [
+        'name' => $faker->name,
+        'price' => $faker->numberBetween(1000 , 100000),
+        'discount_price' => $faker->randomElement($discounts),
+    ];
+});
